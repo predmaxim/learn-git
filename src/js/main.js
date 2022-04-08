9fbc0170ee3e30414641924e10fb61fc2e33a213
Max Pred <predmaxim@ya.ru>
2022-04-08 21:08:43 +0500

    git init

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const a = [1,2,,5,6,7]
+
+let a = null;
+
+
+numbers = 2
+string = 'adasdqweqw'
+bolean = true / false
+array = [0:1,1:2,3:undefined,'aaasd',6,6:[true, 1, 5, 'qweqw']]
+
+
+
+object = {
+  name: undefined, 
+  age: 18,
+
+
+}
+
+
+object.name = 'asdasdasd'
+
+let a:numbers = 'asdsada'
+
+null
+undefined
+
+forEach()
+
+foreach()
+'sadfsd' . 'asdasd'
+
+<br/>
